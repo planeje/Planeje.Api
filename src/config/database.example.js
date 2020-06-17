b77cb69e8c26ab43c32855cea0fc9da333655f5f
zampieri <zampieri@leadlovers.com>
2020-06-16 21:52:30 -0300

    Conexão com o banco no heroku.

--- src/config/database.example.js
+++ src/config/database.example.js
@@ -5,7 +5,10 @@ dotenv.config();
 
 const pool = new Pool({
     //alterar senha
-    connectionString: 'postgresql://postgres:123@localhost:5432/planeje_db'
+    connectionString: 'postgres://lphhwxifkkelbx:f6f9b507c97e9d1a3c02e8e38b1cb987137958ad8ed603c7f14f0194fbb6afab@ec2-34-197-141-7.compute-1.amazonaws.com:5432/d62ihk396ssu2s',
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 pool.on('connect', () => {
