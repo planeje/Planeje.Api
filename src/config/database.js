ff2afb9bb9b0fbedd0e560b260c5e239777a109b
zampieri <zampieri@leadlovers.com>
2020-10-15 22:36:26 -0300

    Configurações de dialect do database.

--- src/config/database.js
+++ src/config/database.js
@@ -5,6 +5,12 @@ module.exports = {
     username: 'lphhwxifkkelbx',
     password:'f6f9b507c97e9d1a3c02e8e38b1cb987137958ad8ed603c7f14f0194fbb6afab',
     database: 'd62ihk396ssu2s',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    },
     define: {
         timestamps: true,
     }
