a21b046a1a9d2e5ec7fe95aa5b2187d7bacb23d1
zampieri <zampieri@leadlovers.com>
2020-10-15 21:17:49 -0300

    Conexão com banco de dados do servidor e procfile.

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,10 @@
 module.exports = {
     dialect: 'postgres',
-    host: 'localhost',
-    username: 'postgres',
-    password:'admin',
-    database: 'planeje_db',
+    host: 'ec2-34-197-141-7.compute-1.amazonaws.com',
+    port: '5432',
+    username: 'lphhwxifkkelbx',
+    password:'f6f9b507c97e9d1a3c02e8e38b1cb987137958ad8ed603c7f14f0194fbb6afab',
+    database: 'd62ihk396ssu2s',
     define: {
         timestamps: true,
     }
