e4e3c63a0ddf95f6a7f1a570d16a854b3cadd2a5
zampieri <zampieri@leadlovers.com>
2020-10-23 01:28:07 -0300

    Ajustes em crud de contas bancárias

--- src/controllers/BankAccountController.js
+++ src/controllers/BankAccountController.js
@@ -3,71 +3,63 @@ const User = require("../models/User");
 
 
 module.exports = {
-    async index( req, res){
-        const { userId } = req.params;
+  async index(req, res) {
+    const { userId } = req.params;
+    const user = await User.findByPk(userId, {
+      include: { association: 'bankAccounts' }
+    });
+    return res.status(200).send(user.bankAccounts);
+  },
 
-        const user = await User.findByPk(userId, {
-            include: { association: 'bankAccounts' }
-        })
+  async store(req, res) {
+    const { userId } = req.params;
+    const { accountName, balance } = req.body;
+    const user = await User.findByPk(userId);
 
-        return res.json(user.bankAccounts)
-    },
-
-    async store(req, res) {
-        const { userId } = req.params;
-        const { accountName, balance } = req.body;
-
-        const user = await User.findByPk(userId);
-
-        if(!user) {
-            return res.status(400).json({ error: 'User not found' });
-        }
-
-        const bankAccount = await BankAccount.create({
-            accountName,
-            balance,
-            userId
-        });
-
-        return res.json(bankAccount);
-    },
-
-    async destroy(req, res){
-        const {id} = req.params;
-        const bankAccount = await BankAccount.findByPk(id)
-        if(bankAccount){
-            BankAccount.destroy({
-                where: {
-                    id: id
-                }
-            })
-            return res.json('Bank Account '+ id + ' deleted');
-        }
-        else{
-            return res.json('Bank Account not found')
-        }
-    },
-
-    async update(req, res){
-        const {id} = req.params;
-        const { accountName, balance } = req.body;
-        const bankAccount = await BankAccount.findByPk(id)
-        if(bankAccount){
-            BankAccount.update({
-                accountName: accountName,
-                balance: balance
-            },{
-                where: {
-                    id: id
-                }
-            })
-            return res.json('Bank Account '+ id + ' updated')
-        }
-        else{
-            return res.json('Bank Account not found')
-        }
+    if(!user) {
+      return res.status(400).json({ error: 'User not found' });
     }
-    
 
+    const bankAccount = await BankAccount.create({
+      accountName,
+      balance,
+      userId
+    });
 
+    return res.status(200).send(bankAccount);
+  },
+
+  async destroy(req, res) {
+    const { id } = req.params;
+    const bankAccount = await BankAccount.findByPk(id);
+
+    if (!bankAccount)
+      return res.status(401).send({ error: 'Bank account not found' });
+
+    BankAccount.destroy({
+      where: {
+        id: id
+      }
+    });
+    return res.status(201).send();
+  },
+
+  async update(req, res) {
+    const { id } = req.params;
+    const { accountName, balance } = req.body;
+    const bankAccount = await BankAccount.findByPk(id);
+
+    if (!bankAccount)
+      return res.status(401).send({ error: 'Bank account not found' });
+
+    const bankAccountEdited = await BankAccount.update({
+      accountName: accountName,
+      balance: balance
+    }, {
+      where: {
+        id: id
+      }
+    });
+    return res.status(200).send(bankAccountEdited);
+  }
 }
--- src/routes.js
+++ src/routes.js
@@ -17,14 +17,14 @@ routes.use(authMiddleware).delete('/users/:id', UserController.destroy);
 routes.use(authMiddleware).put('/users/:id', UserController.update);
 routes.use(authMiddleware).put('/users/:id/changePassword', UserController.updatePassword);
 // bank accounts
-routes.use(authMiddleware).get('/users/:userId/bankAccounts', BankAccountController.index)
-routes.use(authMiddleware).post('/users/:userId/bankAccounts', BankAccountController.store)
-routes.use(authMiddleware).delete('/bankAccounts/:id', BankAccountController.destroy)
-routes.use(authMiddleware).put('/bankAccounts/:id', BankAccountController.update)
+routes.use(authMiddleware).get('/user/:userId/bankAccounts', BankAccountController.index);
+routes.use(authMiddleware).post('/user/:userId/bankAccounts', BankAccountController.store);
+routes.use(authMiddleware).delete('/bankAccounts/:id', BankAccountController.destroy);
+routes.use(authMiddleware).put('/bankAccounts/:id', BankAccountController.update);
 // categories
-routes.use(authMiddleware).get('/users/:userId/categories', CategoryController.index)
-routes.use(authMiddleware).post('/users/:userId/categories', CategoryController.store)
-routes.use(authMiddleware).delete('/categories/:id', CategoryController.destroy)
-routes.use(authMiddleware).put('/categories/:id', CategoryController.update)
+routes.use(authMiddleware).get('/users/:userId/categories', CategoryController.index);
+routes.use(authMiddleware).post('/users/:userId/categories', CategoryController.store);
+routes.use(authMiddleware).delete('/categories/:id', CategoryController.destroy);
+routes.use(authMiddleware).put('/categories/:id', CategoryController.update);
 
 module.exports = routes;
