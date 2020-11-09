19be47643ec13e6680510f4cb5295dfb1dd2e23e
Lucasnhso <47836752+Lucasnhso@users.noreply.github.com>
2020-11-09 20:30:58 -0300

    Get logs trasendo dados de outras tabelas

--- src/controllers/LogController.js
+++ src/controllers/LogController.js
@@ -1,12 +1,40 @@
 const User = require("../models/User");
 const Log = require("../models/Log");
+const BankAccount = require("../models/BankAccount");
+const Category = require("../models/Category");
+const SpendingGoal = require("../models/SpendingGoal");
+const Transaction = require("../models/Transaction");
+const { Op } = require("sequelize");
 
 module.exports = {
     async index(req, res) {
       const { userId } = req.params;
-      const user = await User.findByPk(userId, {
+      let user = await User.findByPk(userId, {
         include: { association: 'logs' }
       });
+
+      let bankAccounts = await BankAccount.findAll({where:{userId},paranoid: false})
+      let categories = await Category.findAll({where:{userId}, paranoid: false})
+      let spendingGoals = await SpendingGoal.findAll({where:{userId}, paranoid:false})
+      let transactions = await Transaction.findAll({where:{userId}, paranoid:false})
+
+      for(item of user.logs){
+        const{ registerId, table } = item
+        item = item.dataValues
+        if(table == 'BankAccounts'){
+          item.bankAccount = bankAccounts.find(e=> e.id == registerId)
+        }
+        else if(table == 'Categories'){
+          item.category = categories.find(e=> e.id == registerId)
+        }
+        else if(table == 'SpendingGoals'){
+          item.spendingGoal = spendingGoals.find(e=> e.id == registerId)
+        }
+        else if(table == 'Transactions'){
+          item.transaction = transactions.find(e=> e.id == registerId)
+        }
+
+      }
       return res.status(200).send(user.logs);
     },
   
