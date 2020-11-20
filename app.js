638e1aa75453ab7167f12e373c799c1e14a34e7b
Lucasnhso <47836752+Lucasnhso@users.noreply.github.com>
2020-11-20 19:26:19 -0300

    Transação futura debita no dia certo

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const routes = require('./src/routes');
 require('dotenv').config();
+const cron = require("node-cron");
+const {schedule} = require('./src/helpers/schedule');
 
 const app = express();
 
@@ -11,5 +13,8 @@ app.use(express.json({ type: 'application/vnd.api+json' }));
 app.use(cors());
 
 app.use(routes);
+
+//cron.schedule("* * * * *", schedule); //executa a cada um minut
+cron.schedule("0 0 0 * * *", schedule, {timezone: "America/Sao_Paulo"}) //executa todo dia as 0hrs
 
 module.exports = app;
---
+++ src/helpers/schedule.js
@@ -0,0 +1,44 @@
+const Transaction = require("../models/Transaction");
+const BankAccount = require("../models/BankAccount");
+const SpendingGoal = require("../models/SpendingGoal");
+const dayjs = require("dayjs");
+const { Op } = require("sequelize");
+const sequelize = require("sequelize");
+
+module.exports = {
+    async schedule(){
+        let transactions = await Transaction.findAll({
+            where: sequelize.where(sequelize.fn('date', sequelize.col('transactionDueDate')), '=', new Date())
+        })
+        
+        for(item of transactions){
+            let bankAccount = await BankAccount.findByPk(item.accountId)
+            if (item.transactionType == 0){
+                await BankAccount.update({
+                    balance: bankAccount.balance - item.transactionValue
+                },{
+                    where:{id: item.accountId}
+                })
+            
+                const spendingGoal = await SpendingGoal.findOne({
+                    where: {categoryId: item.categoryId},
+                    goalDueDate: {[Op.lte] : item.transactionDueDate}
+                });
+                
+                if(spendingGoal){
+                    await SpendingGoal.update(
+                        {valueAvaible: spendingGoal.valueAvaible + item.transactionValue },
+                        {where: { id: spendingGoal.id }}
+                    );
+                }
+            }
+            else{
+                await BankAccount.update({
+                    balance: bankAccount.balance + item.transactionValue
+                },{
+                    where:{id: item.accountId}
+                })
+            }
+        }
+    }
+}
